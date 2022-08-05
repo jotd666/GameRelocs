bdcaf1af26feb42e102c037bae826ebf22cb27d5
Jean-François Fabre <jotd666@gmail.com>
2022-08-06 00:45:01 +0200

    in progress, most tables found & relocated

--- Starglider2/scripts/bin2dcl.py
+++ Starglider2/scripts/bin2dcl.py
@@ -2,9 +2,17 @@ import os,re,struct,defines
 with open("../{}_ref".format(defines.project),"rb") as f:
     contents = f.read()
 
-for i in range(0x061f4,0x06232,4):  # de,fr,uk
-    data = struct.unpack_from(">I",contents,i-0x1000)[0]
+#for i in range(0x0f380,0x0f5d0,4):
+#for i in range(0x1a234,0x1a3d4,4):
+#for i in range(0x394ba,0x39614,4):
+for i in range(0x10544,0x10560,4):
+    data = struct.unpack_from(">I",contents,i-defines.start_org)[0]
+
     if data:
-        print("\tdc.l\tlb_{:05x}\t;{:05x}".format(data,i))
+        if data%2:
+            data = "{:05x}+1".format(data-1)
+        else:
+            data = "{:05x}".format(data)
+        print("\tdc.l\tlb_{}\t;{:05x}".format(data,i))
     else:
         print("\tdc.l\t{}\t;{:05x}".format(data,i))
