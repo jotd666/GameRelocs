a440c41ccf7275f2da906f378c811168927d7046
Jean-François Fabre <jotd666@gmail.com>
2022-11-12 00:33:42 +0100

    found more relocs, unrelocated a lot of sprite & sound data, fastmem with sprites and sound now :)

--- Starglider2/scripts/print_relocs.py
+++ Starglider2/scripts/print_relocs.py
@@ -2,6 +2,7 @@ import struct,collections,itertools,os
 import defines
 import shutil
 
+
 hunk_dict = {0x3F3:"header",0x3E9:"code",0x3EA:"data",0x3F2:"end",0x3EC:"reloc32",0x3EB:"bss",0x3F1:"debug"}
 
 this_dir = os.path.dirname(os.path.abspath(__file__))
@@ -79,7 +80,52 @@ def decode(input_file,binary_file):
         # unreloc file, cancel relocation of labels that should be in chipmem
         # it's much better to compute offsets from labels not to relocate
         derog_labels = {
-            0xD644,  # sprite data
+            0xD644,  # sprite data 0-8 (empty)
+            0x304dc, # spr1
+
+0x2f87c,
+0x2f938,
+0x2f9f8,
+0x2fab8,
+0x2fb78,
+0x2fc38,
+0x2fcf8,
+0x2fdb8,
+0x2fe78,
+0x2fe98,
+0x2fed4,
+0x2fef4,
+0x2ff14,
+0x2ff34,
+0x2ff54,
+0x2ff74,
+0x2ff94,
+0x2ffb4,
+0x2ffd4,
+0x2fff4,
+0x30014,
+0x30034,
+0x30054,
+0x30074,
+0x30094,
+0x300b4,
+0x300d4,
+0x300f4,
+0x30114,
+0x30134,
+0x30154,
+0x30178,
+0x30198,
+0x301b8,
+0x301d8,
+0x301fc,
+0x3021c,
+0x3023c,
+0x3025c,
+0x3027c,
+0x3029c,
+
+
             0x431F4, # a sound buffer
             0xcef8,  # copperlist offsets
             0x0cf20,
@@ -90,6 +136,22 @@ def decode(input_file,binary_file):
             0x0d0b0,
             0x0d0d8,
             0x0d0f0,
+            # audio
+            0x5b8d8,
+            0x5fbc8,
+            0x5ff98,
+            0x4ea76,
+
+            # samples from main sample table in 0x4e5a0
+0x4ea76,
+0x4ea9e,
+0x5b8d8,
+0x5ff98,
+0x623d8,
+0x62c78,
+0x63568,
+0x63e38,
+0x66b58,
             }
         unreloc_offsets = sorted([r for r in reloc_offsets if get_long(binary_contents,r) in derog_labels])
         unreloc_offsets.append(0)  # end with 0
