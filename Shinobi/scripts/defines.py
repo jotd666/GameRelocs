e8b3c6ae8f818832dd41f2e5c6c8e42935c896b1
Jean-François Fabre <jotd666@gmail.com>
2024-05-21 22:46:41 +0200

    started relocating Shinobi (arcade)

---
+++ Shinobi/scripts/defines.py
@@ -0,0 +1,15 @@
+import os
+
+project = "shinobi"
+suffix=""
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+
+binary_file = os.path.join(this_dir,"../{}".format(project))
+asm_file = os.path.join(this_dir,"../{}.s".format(project))
+hunk_file = os.path.join(this_dir,os.pardir,"{}_hunk".format(project))
+
+
+start_org = 0x0
+code_start = 0x819C
+end_address = 0x531f2
